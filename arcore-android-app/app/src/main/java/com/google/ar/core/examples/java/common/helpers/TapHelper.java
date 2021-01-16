0a6716c556036005ef1eda5e616bbb56c9c7e921
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2021-01-16 09:49:21 -0500

    Fix dependencies

--- arcore-android-app/app/src/main/java/com/google/ar/core/examples/java/common/helpers/TapHelper.java
+++ arcore-android-app/app/src/main/java/com/google/ar/core/examples/java/common/helpers/TapHelper.java
@@ -20,6 +20,7 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -28,44 +29,44 @@ import java.util.concurrent.BlockingQueue;
  * render thread.
  */
 public final class TapHelper implements OnTouchListener {
-  private final GestureDetector gestureDetector;
-  private final BlockingQueue<MotionEvent> queuedSingleTaps = new ArrayBlockingQueue<>(16);
+    private final GestureDetector gestureDetector;
+    private final BlockingQueue<MotionEvent> queuedSingleTaps = new ArrayBlockingQueue<>(16);
 
-  /**
-   * Creates the tap helper.
-   *
-   * @param context the application's context.
-   */
-  public TapHelper(Context context) {
-    gestureDetector =
-        new GestureDetector(
-            context,
-            new GestureDetector.SimpleOnGestureListener() {
-              @Override
-              public boolean onSingleTapUp(MotionEvent e) {
-                // Queue tap if there is space. Tap is lost if queue is full.
-                queuedSingleTaps.offer(e);
-                return true;
-              }
+    /**
+     * Creates the tap helper.
+     *
+     * @param context the application's context.
+     */
+    public TapHelper(Context context) {
+        gestureDetector =
+                new GestureDetector(
+                        context,
+                        new GestureDetector.SimpleOnGestureListener() {
+                            @Override
+                            public boolean onSingleTapUp(MotionEvent e) {
+                                // Queue tap if there is space. Tap is lost if queue is full.
+                                queuedSingleTaps.offer(e);
+                                return true;
+                            }
 
-              @Override
-              public boolean onDown(MotionEvent e) {
-                return true;
-              }
-            });
-  }
+                            @Override
+                            public boolean onDown(MotionEvent e) {
+                                return true;
+                            }
+                        });
+    }
 
-  /**
-   * Polls for a tap.
-   *
-   * @return if a tap was queued, a MotionEvent for the tap. Otherwise null if no taps are queued.
-   */
-  public MotionEvent poll() {
-    return queuedSingleTaps.poll();
-  }
+    /**
+     * Polls for a tap.
+     *
+     * @return if a tap was queued, a MotionEvent for the tap. Otherwise null if no taps are queued.
+     */
+    public MotionEvent poll() {
+        return queuedSingleTaps.poll();
+    }
 
-  @Override
-  public boolean onTouch(View view, MotionEvent motionEvent) {
-    return gestureDetector.onTouchEvent(motionEvent);
-  }
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return gestureDetector.onTouchEvent(motionEvent);
+    }
 }
